51e54ca600d69755f44e1bcf7454d4347e414eb7
phasan <phasan@NIS-PK639-MacBook-Pro.local>
2022-06-22 10:33:12 +0500

    initial commit

---
+++ src/test/java/com/nisum/employeemanagementsystem/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.employeemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
